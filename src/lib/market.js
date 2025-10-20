be09a79bbfcc82c91b40d0b53df38f8a41d6aada
Crusen <hakan2altun@gmail.com>
2025-10-20 20:55:40 +0300

    güncelleme

--- src/lib/market.js
+++ src/lib/market.js
@@ -1,4 +1,5 @@
-import { db as _db, getDB } from "./db.js";
+import { getDB } from "./db.js";
+
 
 // Seeded RNG (mulberry32) + normal distro
 function mulberry32(a){return function(){let t=a+=0x6D2B79F5;t=Math.imul(t^t>>>15,t|1);t^=t+Math.imul(t^t>>>7,t|61);return ((t^t>>>14)>>>0)/4294967296;};}
